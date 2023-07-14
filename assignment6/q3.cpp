595c4e873ab544bb44216c63e5e2d9aa39645abe
Pushpam Kumar Jha <135735962+PushpamJha14@users.noreply.github.com>
2023-07-14 23:32:34 +0530

    Update q3.cpp

--- assignment6/q3.cpp
+++ assignment6/q3.cpp
@@ -2,30 +2,47 @@
 
 using namespace std;
 
+bool validMountainArray(vector<int> &arr)
+{
+    int n = arr.size();
+    if (n < 3)
+    {
+        return false;
+    }
+    int i = 0;
+    while (i < n - 1 && arr[i] < arr[i + 1])
+    {
+        i++;
+    }
+    if (i == 0 || i == n - 1)
+    {
+        return false;
+    }
+
+    while (i < n - 1 && arr[i] > arr[i + 1])
+    {
+        i++;
+    }
+
+    return i == n - 1;
+}
+
 int main()
 {
-    vector<int> nums = {-4,
-                        -1,
-                        0,
-                        3,
-                        10};
-    int n = nums.size(), i = 0, j = n - 1, k = n - 1;
-    vector<int> ans(n);
-    while (i < j)
-    {
-        if (abs(nums[i]) > abs(nums[j]))
-        {
-            ans[k] = nums[i] * nums[i];
-            i++;
-            k--;
-        }
-        else
-        {
-            ans[k] = nums[j] * nums[j];
-            j--;
-            k--;
-        }
-    }
-    for (int i = 0; i < n; i++)
-        cout << ans[i] << " ";
+    vector<int> arr1 = {2, 1};
+    cout << boolalpha << validMountainArray(arr1) << endl; // false, less than 3 elements
+
+    vector<int> arr2 = {3, 5, 5};
+    cout << boolalpha << validMountainArray(arr2) << endl; // false, no peak
+
+    vector<int> arr3 = {0, 3, 2, 1};
+    cout << boolalpha << validMountainArray(arr3) << endl; // true
+
+    vector<int> arr4 = {0, 2, 3, 4, 5, 2, 1, 0};
+    cout << boolalpha << validMountainArray(arr4) << endl; // true
+
+    vector<int> arr5 = {0, 2, 3, 3, 5, 2, 1, 0};
+    cout << boolalpha << validMountainArray(arr5) << endl; // false, plateau in the ascending part
+
+    return 0;
 }
