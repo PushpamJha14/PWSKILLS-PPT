0e8240bed48dec38daf70b4ab1194e6d719d6812
Pushpam Kumar Jha <135735962+PushpamJha14@users.noreply.github.com>
2023-07-15 00:47:27 +0530

    Update q8.cpp

--- assignment6/q8.cpp
+++ assignment6/q8.cpp
@@ -2,37 +2,63 @@
 
 using namespace std;
 
-vector<int> findOriginalArray(vector<int> &a)
+vector<vector<int>> multiply(vector<vector<int>> &mat1, vector<vector<int>> &mat2)
 {
-    if (a.size() % 2)
-        return {};
-    vector<int> res;
-    sort(a.begin(), a.end());
-    unordered_map<int, int> mp;
-    for (int i = 0; i < a.size(); i++)
+    int m = mat1.size();
+    int k = mat1[0].size();
+    int n = mat2[0].size();
+
+    vector<vector<int>> result(m, vector<int>(n, 0));
+
+    unordered_map<int, unordered_map<int, int>> mat2Map;
+    for (int i = 0; i < k; i++)
     {
-        mp[a[i]]++;
+        for (int j = 0; j < n; j++)
+        {
+            if (mat2[i][j] != 0)
+            {
+                mat2Map[j][i] = mat2[i][j];
+            }
+        }
     }
-    for (int i = 0; i < a.size(); i++)
+
+    for (int i = 0; i < m; i++)
     {
-        if (mp[a[i]] == 0)
-            continue;
-        if (mp[a[i] * 2] == 0)
-            return {};
-        res.push_back(a[i]);
-        mp[a[i]]--;
-        mp[a[i] * 2]--;
+        for (int j = 0; j < n; j++)
+        {
+            if (mat1[i][j] != 0)
+            {
+                for (auto it = mat2Map[j].begin(); it != mat2Map[j].end(); it++)
+                {
+                    result[i][it->first] += mat1[i][j] * it->second;
+                }
+            }
+        }
     }
-    return res;
+
+    return result;
 }
 
 int main()
 {
-    vector<int> nums = {1, 3, 4, 2, 6, 8};
-    vector<int> ans = findOriginalArray(nums);
-    for (int num : ans)
+    vector<vector<int>> mat1 = {
+        {1, 0, 0},
+        {-1, 0, 3}};
+    vector<vector<int>> mat2 = {
+        {7, 0, 0},
+        {0, 0, 0},
+        {0, 0, 1}};
+
+    vector<vector<int>> result = multiply(mat1, mat2);
+
+    for (const auto &row : result)
     {
-        cout << num << " ";
+        for (int num : row)
+        {
+            cout << num << " ";
+        }
+        cout << endl;
     }
+
     return 0;
 }
